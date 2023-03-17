54020b56f3537b6c2f2c82d7d1ff66ed4fe7c627
rubuz <rubuz23@gmail.com>
2023-03-17 13:45:17 +0100

    css and button which rolls dice

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
 import { useState } from "react";
+import Die from "./components/Die";
 
 function App() {
+  const [dice, setDice] = useState(allNewDice());
+
+  function allNewDice() {
+    const newDice = [];
+    for (let i = 0; i < 10; i++) {
+      newDice.push(Math.ceil(Math.random() * 6));
+    }
+    return newDice;
+  }
+
+  function rollDice() {
+    setDice((prevDice) => allNewDice());
+  }
+
+  const diceElements = dice.map((item) => <Die value={item} />);
+
   return (
     <main>
-      <h1>test</h1>
+      <div className="dice-container">{diceElements}</div>
+      <button className="roll-btn" onClick={rollDice}>
+        ROLL
+      </button>
     </main>
   );
 }
