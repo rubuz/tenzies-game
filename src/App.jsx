2f9ae81e69aab8c8e80b06b78023c1cb6a159b78
Matjaz Lovenjak <rubuz23@gmail.com>
2023-03-17 19:24:58 +0100

    added win condition

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,51 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { nanoid } from "nanoid";
 import Die from "./components/Die";
 
 function App() {
   const [dice, setDice] = useState(allNewDice());
+  const [tenzies, setTenzies] = useState(false);
+
+  useEffect(() => {
+    const allHeld = dice.every((die) => die.isHeld);
+    const firstValue = dice[0].value;
+    const allSameValue = dice.every((die) => die.value === firstValue);
+    if (allHeld && allSameValue) {
+      setTenzies(true);
+      console.log("You won!");
+    }
+  }, [dice]);
 
   function allNewDice() {
     const newDice = [];
     for (let i = 0; i < 10; i++) {
-      const number = Math.ceil(Math.random() * 6);
-      newDice.push({
-        value: number,
-        isHeld: true,
-        id: nanoid(),
-      });
+      newDice.push(generateNewDie());
     }
     return newDice;
   }
 
+  function generateNewDie() {
+    return {
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: nanoid(),
+    };
+  }
+
   function holdDice(id) {
-    console.log(id);
+    setDice((oldDice) =>
+      oldDice.map((die) => {
+        return die.id === id ? { ...die, isHeld: !die.isHeld } : die;
+      })
+    );
   }
 
   function rollDice() {
-    setDice((prevDice) => allNewDice());
+    setDice((prevDice) =>
+      prevDice.map((die) => {
+        return die.isHeld ? die : generateNewDie();
+      })
+    );
   }
 
   const diceElements = dice.map((item) => (
@@ -37,6 +59,14 @@ function App() {
 
   return (
     <main>
+      <div className="text">
+        <h1 className="title">Tenzies</h1>
+        <p className="instructions">
+          Roll until all dice are the same. Click each die to freeze it at its
+          current value between rolls.
+        </p>
+      </div>
+
       <div className="dice-container">{diceElements}</div>
       <button className="roll-btn" onClick={rollDice}>
         Roll
