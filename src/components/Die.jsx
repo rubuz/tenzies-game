db6f6f34225c8e1de2cbfa4f1c2f34f9aaf81c7c
rubuz <rubuz23@gmail.com>
2023-03-21 12:58:48 +0100

    dice faces working

--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -4,14 +4,93 @@ function Die(props) {
   const styles = {
     backgroundColor: props.isHeld ? "#59E391" : "white",
   };
+  const dieValue = props.value;
 
-  function DieFace() {}
+  const DieFace = () => {
+    switch (dieValue) {
+      case 1:
+        return (
+          <div
+            style={styles}
+            className="die first-face"
+            onClick={props.holdDice}
+          >
+            <span className="dot"> </span>
+          </div>
+        );
+      case 2:
+        return (
+          <div
+            style={styles}
+            className="die second-face"
+            onClick={props.holdDice}
+          >
+            <span className="dot"> </span>
+            <span className="dot"> </span>
+          </div>
+        );
+      case 3:
+        return (
+          <div
+            style={styles}
+            className="die third-face"
+            onClick={props.holdDice}
+          >
+            <span className="dot"> </span>
+            <span className="dot"> </span>
+            <span className="dot"> </span>
+          </div>
+        );
+      case 4:
+        return (
+          <div
+            style={styles}
+            className="die fourth-face"
+            onClick={props.holdDice}
+          >
+            <span className="dot"> </span>
+            <span className="dot"> </span>
+            <span className="dot"> </span>
+            <span className="dot"> </span>
+          </div>
+        );
+      case 5:
+        return (
+          <div
+            style={styles}
+            className="die fifth-face"
+            onClick={props.holdDice}
+          >
+            <span className="dot"> </span>
+            <span className="dot"> </span>
+            <span className="dot"> </span>
+            <span className="dot"> </span>
+            <span className="dot"> </span>
+          </div>
+        );
+      case 6:
+        return (
+          <div
+            style={styles}
+            className="die sixth-face"
+            onClick={props.holdDice}
+          >
+            <span className="dot"> </span>
+            <span className="dot"> </span>
+            <span className="dot"> </span>
+            <span className="dot"> </span>
+            <span className="dot"> </span>
+            <span className="dot"> </span>
+          </div>
+        );
+    }
+  };
 
   return (
-    <div style={styles} className="die first-face" onClick={props.holdDice}>
-      {/* {props.value} */}
-      <span className="dot"> </span>
-    </div>
+    // <div style={styles} className="die" onClick={props.holdDice}>
+    //   {props.value}
+    // </div>
+    <>{DieFace()}</>
   );
 }
 
